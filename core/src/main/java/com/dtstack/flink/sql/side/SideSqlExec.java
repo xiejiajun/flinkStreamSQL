9d8d4a7644e0bf154c6ac5cfff6c11636e36e2fc
蒋俊杰 <jiangjunjie@jiangjunjiedeMacBook-Pro.local>
2018-11-09 11:06:47 +0800

    bugfix

--- core/src/main/java/com/dtstack/flink/sql/side/SideSqlExec.java
+++ core/src/main/java/com/dtstack/flink/sql/side/SideSqlExec.java
@@ -192,7 +192,7 @@ public class SideSqlExec {
         rightScopeChild.setTableName(joinInfo.getRightTableName());
         SideTableInfo sideTableInfo = sideTableMap.get(joinInfo.getRightTableName());
         if(sideTableInfo == null){
-            sideTableInfo = sideTableMap.get(joinInfo.getRightTableName());
+            sideTableInfo = sideTableMap.get(joinInfo.getRightTableAlias());
         }
 
         if(sideTableInfo == null){
